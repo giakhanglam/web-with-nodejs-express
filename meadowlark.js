270128e9dc466d0d46b2bb5852376f943e2064d6
giakhanglam <145giakhang@gmail.com>
2018-02-01 23:32:56 +0700

    please work first commit

---
+++ meadowlark.js
@@ -0,0 +1,53 @@
+var express = require('express'),
+	app = express();
+
+// set up handlebars view engine, replace ejs
+var handlebars = require('express3-handlebars').create({ defaultLayout: 'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+// add STATIC middleware
+app.use(express.static(__dirname + '/public'));
+
+app.set('port', process.env.PORT || 3000);
+
+// array of fortune cookies
+var fortunes = [
+	"Conquer your fears or they will conquer you.",
+	"Rivers need springs.",
+	"Do not fear what you don't know.",
+	"You will have a pleasant surprise.",
+	"Whenever possible, keep it simple."
+];
+
+
+
+app.get('/', function(req, res){
+	res.render('home');
+});
+
+app.get('/about', function(req, res){
+	var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+	res.render('about', {fortune: randomFortune});
+});
+
+/*
+ * middlewares, catch-all handlers, need to specify the status code
+ */
+// custom 404 page
+app.use(function(req, res){
+	res.status(404);
+	res.render('404');
+});
+
+// custom 500 page
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+	console.log('Express started on http://localhost:' + 
+		app.get('port') + '; press Ctrl-C to terminate.');
+});
