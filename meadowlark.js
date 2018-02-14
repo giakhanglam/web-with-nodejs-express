4fda3063490904f374d835f611ef621981310a09
giakhanglam <145giakhang@gmail.com>
2018-02-14 09:29:32 +0700

    added partials weather

--- meadowlark.js
+++ meadowlark.js
@@ -24,6 +24,14 @@ app.use(function(req, res, next){
 	next();
 });
 
+// middleware to inject weather data into res.locals.partials
+app.use(function(req, res, next){
+	if(!res.locals.partials) res.locals.partials = {};
+	res.locals.partials.weather = getWeatherData();
+	next();
+});
+
+
 // ROUTES
 
 app.get('/', function(req, res){
@@ -35,6 +43,14 @@ app.get('/about', function(req, res){
 		fortune: fortune.getFortune(),
 		pageTestScript: '/qa/tests-about.js'
 	});
+});
+
+// this route show 'invisible' info that the browser sends to server when request
+app.get('/headers', function(req, res){
+	res.set('Content-Type', 'text/plain');
+	var s = '';
+	for(var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
+	res.send(s);
 });
 
 app.get('/tours/hood-river', function(req, res){
@@ -49,18 +65,46 @@ app.get('/tours/request-group-rate', function(req, res){
  */
 // custom 404 page
 app.use(function(req, res){
-	res.status(404);
-	res.render('404');
+	res.status(404).render('404');
 });
 
 // custom 500 page
 app.use(function(err, req, res, next){
 	console.error(err.stack);
-	res.status(500);
-	res.render('500');
+	res.status(500).render('500');
 });
 
+
 if(app.thing === null) console.log('bleat!');
+
+// function to get current weather data
+function getWeatherData(){
+	return {
+		locations: [
+			{
+				name: 'Portland',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+				weather: 'Overcast',
+				temp: '54.1 F (12.3 C)'
+			},
+			{
+				name: 'Bend',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+				weather: 'Partly Cloudy',
+				temp: '55.0 F (12.8 C)'
+			},
+			{
+				name: 'Manzanita',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+				weather: 'Light Rain',
+				temp: '55.0 F (12.8 C)'
+			}
+		]
+	};
+}
 
 app.listen(app.get('port'), function(){
 	console.log('Express started on http://localhost:' + 
