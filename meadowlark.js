4dca1251f46ad4af7799726a18d2fbb062b4b253
giakhanglam <145giakhang@gmail.com>
2018-02-27 01:59:25 +0700

    chap8-form,p1

--- meadowlark.js
+++ meadowlark.js
@@ -2,7 +2,18 @@ var express = require('express'),
 	app = express();
 
 // set up handlebars view engine, replace ejs
-var handlebars = require('express3-handlebars').create({ defaultLayout: 'main' });
+var handlebars = require('express3-handlebars').create({ 
+	defaultLayout: 'main',
+	helpers: {
+		section: function(name, options){
+			if(!this._sections) this._sections = {};
+			this._sections[name] = options.fn(this);
+			return null;
+		}
+	}
+});
+
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
@@ -11,6 +22,9 @@ var fortune = require('./lib/fortune.js');
 
 // add STATIC middleware
 app.use(express.static(__dirname + '/public'));
+
+// add body-parser middleware for POST form
+app.use(require('body-parser')());
 
 app.set('port', process.env.PORT || 3000);
 
@@ -43,6 +57,32 @@ app.get('/about', function(req, res){
 		fortune: fortune.getFortune(),
 		pageTestScript: '/qa/tests-about.js'
 	});
+});
+
+// this route just show about Sections page 79
+app.get('/test-sections', function(req, res){
+	res.render('jquery-test');
+});
+
+app.get('/newsletter', function(req, res){
+	// we will learn about CSRF later...for now, just
+	// provide a dummy value
+	res.render('newsletter', {csrf: 'CSRF token goes here'});
+});
+
+app.post('/process', function(req, res){
+	/*console.log('Form (from querystring): ' + req.query.form);
+	console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+	console.log('Name (from visible form field): ' + req.body.name);
+	console.log('Email (from visible form field): ' + req.body.email);
+	res.redirect(303, '/thank-you');*/
+	if(req.xhr || req.accepts('json,html')==='json'){
+		// if there were an error, we would send { error: 'error description' }
+		res.send({success: true});
+	} else {
+		// if there were an error, we would redirect to an error page
+		res.redirect(303, '/thank-you');
+	}
 });
 
 // this route show 'invisible' info that the browser sends to server when request
